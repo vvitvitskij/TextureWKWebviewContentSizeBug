b54e83fcbd72f2f2d46c9ce508710918c82e4f43
vvitvitskij <vvitvitskij@naumen.ru>
2019-07-22 11:33:45 +0500

    Initial Commit

---
+++ WKWebviewContentSizeBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKWebviewContentSizeBug
+//
+//  Created by Vadim Vitvickiy on 22/07/2019.
+//  Copyright © 2019 Vadim Vitvickiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
